2799a8afb5de001900552048a7c1353f43eadd45
Thomas Warford <thomasdwarford@hotmail.co.uk>
2023-11-07 17:28:10 +0000

    Remove prints, use view_prediction

--- src/band_plotters.py
+++ src/band_plotters.py
@@ -124,8 +124,6 @@ def plot_from_bands_tensor(material_id, band_energies_tensor_normalized, data_di
     band_energies_minus_efermi = band_energies_tensor_normalized.detach().cpu().numpy()
     band_energies_minus_efermi = band_energies_minus_efermi * (MAX_ENERGY_MINUS_EFERMI_NEAR_EFERMI - MIN_ENERGY_MINUS_EFERMI_NEAR_EFERMI) + MIN_ENERGY_MINUS_EFERMI_NEAR_EFERMI
     
-    print(band_energies_minus_efermi)
-    
     return plot_from_bands_picture(material_id, band_energies_minus_efermi, data_directory=data_directory, e_bounds=e_bounds, verbose=verbose)
     
 def view_prediction(material_id, model, data_directory=DATA_DIRECTORY, e_bounds=[-4, 4], verbose=True):
@@ -135,7 +133,6 @@ def view_prediction(material_id, model, data_directory=DATA_DIRECTORY, e_bounds=
     input_tensor = torch.from_numpy(load_tiff_uint32_image(image_filename).astype(np.float64))
     input_tensor = input_tensor / (2**16-1)
     input_tensor = input_tensor[None, None, :, :]
-    print(input_tensor)
     input_tensor = input_tensor.float().cuda()
     output_tensor = model.forward(input_tensor)
     
