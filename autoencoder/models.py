064e1fe13244acc11100afa518d9cb51e0f7ae22
Thomas Warford <thomasdwarford@hotmail.co.uk>
2023-11-30 15:17:27 +0000

    add (flawed) resnetvaeold

--- autoencoder/models.py
+++ autoencoder/models.py
@@ -61,3 +61,70 @@ class SimpleAutoencoder(nn.Module):
         out = self.decode_linear(out)
         out = out.view(-1, 8, 3, 32)
         return self.decode(out)
+    
+class SimpleAutoencoder128(nn.Module):
+    def __init__(self, n_latent=128, norm=False):
+        super().__init__()
+        self.encode = nn.Sequential(
+            conv(1, 4, norm=norm), # 64x64
+            conv(4, 8, norm=norm), # 32x32
+            nn.Flatten(),
+            nn.Linear(32*32*8, n_latent),
+            nn.Tanh(),
+        )
+        
+        self.decode_linear=nn.Sequential(
+            nn.Linear(n_latent, 32*32*8),
+            nn.BatchNorm1d(32*32*8),
+            nn.ReLU(),
+        )
+        
+        self.decode = nn.Sequential(
+            deconv(8, 4, norm=norm), # 6x64
+            deconv(4, 1, norm=norm, act=False), # 12x128
+            nn.Sigmoid()
+        )
+        
+    
+    def forward(self, x):
+        out = self.encode(x)
+        out = self.decode_linear(out)
+        out = out.view(-1, 8, 32, 32)
+        return self.decode(out)
+    
+class SimpleAutoencoderDeep128(nn.Module):
+    def __init__(self, n_latent=128, norm=False):
+        super().__init__()
+        
+        self.max_channels = 32
+        self.min_height = 16
+        lin_size = self.max_channels*self.min_height**2
+        
+        self.encode = nn.Sequential(
+            conv(1, 8, norm=norm), # 64x64
+            conv(8, 16, norm=norm), # 32x32
+            conv(16, 32, norm=norm), # 16x16
+            nn.Flatten(),
+            nn.Linear(lin_size, n_latent),
+            nn.Tanh(),
+        )
+        
+        self.decode_linear=nn.Sequential(
+            nn.Linear(n_latent, lin_size),
+            nn.BatchNorm1d(lin_size),
+            nn.ReLU(),
+        )
+        
+        self.decode = nn.Sequential(
+            deconv(32, 16, norm=norm), # 6x64
+            deconv(16, 8, norm=norm), # 6x64
+            deconv(8, 1, norm=norm, act=False), # 12x128
+            nn.Sigmoid()
+        )
+        
+    
+    def forward(self, x):
+        out = self.encode(x)
+        out = self.decode_linear(out)
+        out = out.view(-1, self.max_channels, self.min_height, self.min_height)
+        return self.decode(out)
